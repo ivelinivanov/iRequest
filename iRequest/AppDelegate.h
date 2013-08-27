79a168d9b3f3339b2476d2776d1265a27724e4af
Ivelin Ivanov <ivelin.ivanov@Student01s-Mac-mini.local>
2013-08-27 14:26:09 +0300

    Initial Commit

---
+++ iRequest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iRequest
+//
+//  Created by Ivelin Ivanov on 8/27/13.
+//  Copyright (c) 2013 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iRequestTests/iRequestTests.m
@@ -0,0 +1,34 @@
+//
+//  iRequestTests.m
+//  iRequestTests
+//
+//  Created by Ivelin Ivanov on 8/27/13.
+//  Copyright (c) 2013 MentorMate. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iRequestTests : XCTestCase
+
+@end
+
+@implementation iRequestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
